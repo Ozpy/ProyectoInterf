0191685bfce27ccaf0407f97595a3708d3f73781
Ozpy <oscarale46@gmail.com>
2020-06-15 18:30:26 -0500

    Agrega usuarios y direccion a base de datos local.

---
+++ app/src/main/java/com/example/proyectointerf/BD/AdminSQLiteOpenHelper.java
@@ -0,0 +1,23 @@
+package com.example.proyectointerf.BD;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super((Context) context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase BaseDeDatos) {
+        BaseDeDatos.execSQL("create table usuario(id_firebase text primary key ,nombre text,correo text, calle text,colonia text, codigopostal text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
